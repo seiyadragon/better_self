53892fc70baf26099c05563ce27b60d288e07845
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-12-28 20:48:57 -0600

    0.2.0

--- components/habit.tsx
+++ components/habit.tsx
@@ -25,23 +25,23 @@ const Habit = ({name, days, id}: HabitProps) => {
 
     return (
         <section className="flex text-white my-4 text-base">
-            <p className="bg-gray-700 w-36 h-36 py-4 px-4 text-center overflow-x-hidden">{finalName}</p>
-            <p className="bg-gray-700 w-36 h-36 py-4 px-4 ml-4 text-center overflow-x-hidden">{days} days</p>
-            <section className="ml-4 w-36 h-36">
+            <p className="bg-blue-600 w-36 h-36 py-4 px-4 text-center overflow-x-hidden shadow-lg">{finalName}</p>
+            <p className="bg-blue-600 w-36 h-36 py-4 px-4 ml-4 text-center overflow-x-hidden shadow-lg">{days} days</p>
+            <section className="ml-4 w-36 h-36 shadow-lg">
                 <button 
-                    className="w-full h-1/3 bg-green-400 px-3 hover:scale-105 transition-transform flex justify-center items-center"
+                    className="w-full h-1/3 bg-green-600 px-3 hover:scale-105 transition-transform flex justify-center items-center"
                     onClick={async () => await supabaseClient.from("UserHabits").update({days: days + 1}).eq("id", id)}
                 >
                     <FaArrowUp />
                 </button>
                 <button 
-                    className="w-full h-1/3 bg-orange-400 px-3 hover:scale-105 transition-transform flex justify-center items-center"
+                    className="w-full h-1/3 bg-orange-600 px-3 hover:scale-105 transition-transform flex justify-center items-center"
                     onClick={async () => await supabaseClient.from("UserHabits").update({days: 0}).eq("id", id)}
                 >
                     <FaRedoAlt />
                 </button>
                 <button 
-                    className="w-full h-1/3 bg-red-400 px-3 hover:scale-105 transition-transform flex justify-center items-center" 
+                    className="w-full h-1/3 bg-red-600 px-3 hover:scale-105 transition-transform flex justify-center items-center" 
                     onClick={async () => await supabaseClient.from("UserHabits").delete().eq("id", id)}
                 >
                     <FaBan/>
--- components/task_manager.tsx
+++ components/task_manager.tsx
@@ -48,88 +48,104 @@ const TaskManager = ({date}: TaskManagerProps) => {
         return (
             <section className="flex flex-col py-8 text-white">
                 <textarea 
-                    className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg" 
+                    className="bg-orange-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg placeholder-white" 
                     placeholder="Use this box to write about yesterday, your dreams, goals, plans for the day, whatever you want. It's about you!"
-                    rows={3}
+                    rows={4}
                     value={textArea1}
                     onChange={async (event: ChangeEvent<HTMLTextAreaElement>) => setTextArea1(event.currentTarget.value)}
                 />
                 <textarea 
-                    className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg" 
+                    className="bg-green-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg placeholder-white" 
                     placeholder="Use this box to write about your health, plan your meals, exercises you will do, etc..."
-                    rows={3}
+                    rows={4}
                     value={textArea2}
                     onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setTextArea2(event.currentTarget.value)}
                 />
                 <textarea 
-                    className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg" 
+                    className="bg-blue-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg placeholder-white" 
                     placeholder="Use this box to write about any goals you want to accomplish, be it short or long term, or even just an idea, just write it down."
-                    rows={3}
+                    rows={4}
                     value={textArea3}
                     onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setTextArea3(event.currentTarget.value)}
                 />
                 <textarea 
-                    className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg" 
+                    className="bg-red-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg placeholder-white" 
                     placeholder="Use this box to write about any problems you are currently facing and how you will solve them. Don't just complain, but think."
-                    rows={3}
+                    rows={4}
                     value={textArea4}
                     onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setTextArea4(event.currentTarget.value)}
                 />
                 <textarea 
-                    className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg" 
+                    className="bg-purple-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg placeholder-white" 
                     placeholder="Use this box to write about your accomplishments, things you're happy about. Forget being humble for your efforts must be celebrated."
-                    rows={3}
+                    rows={4}
                     value={textArea5}
                     onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setTextArea5(event.currentTarget.value)}
                 />
-                <button 
-                    className="py-8 px-16 my-16 bg-gray-700 hover:bg-gray-600 shadow-lg text-2xl text-orange-400 self-center" 
-                    onClick={async () => {
-                        setIsEdit(!isEdit)
-                        const {error} = await supabaseClient.from("UserRoutines").delete().eq("date", date)
+                <section className="self-center">
+                    <button 
+                        className="py-8 px-16 my-16 mx-8 bg-green-800 hover:bg-green-700 shadow-lg text-2xl text-orange-400" 
+                        onClick={async () => {
+                            if (!textArea1 || !textArea2 || !textArea4 || !textArea5) {
+                                alert("Please fill out all the fields!")
+                                return
+                            }
 
-                        await supabaseClient.from("UserRoutines").insert({
-                        userid: user !== null ? user.id : null,
-                        textBoxes: [
-                            textArea1,
-                            textArea2,
-                            textArea3,
-                            textArea4,
-                            textArea5
-                        ],
-                        date: date
-                    })}}
-                >
-                    Save!
-                </button>
+                            setIsEdit(!isEdit)
+                            const {error} = await supabaseClient.from("UserRoutines").delete().eq("date", date)
+
+                            await supabaseClient.from("UserRoutines").insert({
+                            userid: user !== null ? user.id : null,
+                            textBoxes: [
+                                textArea1,
+                                textArea2,
+                                textArea3,
+                                textArea4,
+                                textArea5
+                            ],
+                            date: date
+                        })}}
+                    >
+                        Save!
+                    </button>
+                    <button 
+                        className="py-8 px-16 my-16 mx-8 bg-red-800 hover:bg-red-700 shadow-lg text-2xl text-orange-400" 
+                        onClick={async () => {
+                            setIsEdit(!isEdit)
+                            const {error} = await supabaseClient.from("UserRoutines").delete().eq("date", date)
+                        }}
+                    >
+                        Cancel!
+                    </button>
+                </section>
             </section>
         )
 
     else {
         return (
             <section className="flex flex-col py-8 text-white">
-                <p className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-24 overflow-y-scroll">
+                <p className="bg-orange-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-32 overflow-y-scroll">
                     {data.length > 0 && data[0].textBoxes[0]}
                     {data.length <= 0 && "Use this box to write about yesterday, your dreams, goals, plans for the day, whatever you want. It's about you!"}
                 </p>
-                <p className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-24 overflow-y-scroll">
+                <p className="bg-green-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-32 overflow-y-scroll">
                     {data.length > 0 && data[0].textBoxes[1]}
                     {data.length <= 0 && "Use this box to write about your health, plan your meals, exercises you will do, etc..."}
                 </p>
-                <p className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-24 overflow-y-scroll">
+                <p className="bg-blue-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-32 overflow-y-scroll">
                     {data.length > 0 && data[0].textBoxes[2]}
                     {data.length <= 0 && "Use this box to write about any goals you want to accomplish, be it short or long term, or even just an idea, just write it down."}
                 </p>
-                <p className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-24 overflow-y-scroll">
+                <p className="bg-red-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-32 overflow-y-scroll">
                     {data.length > 0 && data[0].textBoxes[3]}
                     {data.length <= 0 && "Use this box to write about any problems you are currently facing and how you will solve them. Don't just complain, but think."}
                 </p>
-                <p className="bg-gray-700 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-24 overflow-y-scroll">
+                <p className="bg-purple-600 outline-none text-white resize-none my-4 px-4 py-4 shadow-lg h-32 overflow-y-scroll">
                     {data.length > 0 && data[0].textBoxes[4]}
                     {data.length <= 0 && "Use this box to write about your accomplishments, things you're happy about. Forget being humble for your efforts must be celebrated."}
                 </p>
                 <button 
-                    className="py-8 px-16 my-16 bg-gray-700 hover:bg-gray-600 shadow-lg text-2xl text-orange-400 self-center text-center" 
+                    className="py-8 px-16 my-16 bg-green-800 hover:bg-green-700 shadow-lg text-2xl text-orange-400 self-center text-center" 
                     onClick={() => setIsEdit(!isEdit)}
                 >
                     Edit!
