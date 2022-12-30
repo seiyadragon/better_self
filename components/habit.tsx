d4cf2432fb774eb45491203e149a090241b86213
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-12-30 01:41:39 -0600

    Update habit.tsx

--- components/habit.tsx
+++ components/habit.tsx
@@ -25,9 +25,9 @@ const Habit = ({name, days, id}: HabitProps) => {
 
     return (
         <section className="flex text-white my-4 text-base">
-            <p className="bg-blue-600 w-36 h-36 py-4 px-4 text-center overflow-x-hidden shadow-lg">{finalName}</p>
-            <p className="bg-blue-600 w-36 h-36 py-4 px-4 ml-4 text-center overflow-x-hidden shadow-lg">{days} days</p>
-            <section className="ml-4 w-36 h-36 shadow-lg">
+            <p className="bg-blue-600 md:w-36 w-24 h-36 py-4 px-4 text-center overflow-x-hidden shadow-lg">{finalName}</p>
+            <p className="bg-blue-600 md:w-36 w-24 h-36 py-4 px-4 ml-4 text-center overflow-x-hidden shadow-lg">{days} days</p>
+            <section className="ml-4 md:w-36 w-24 h-36 shadow-lg">
                 <button 
                     className="w-full h-1/3 bg-green-600 px-3 hover:scale-105 transition-transform flex justify-center items-center"
                     onClick={async () => await supabaseClient.from("UserHabits").update({days: days + 1}).eq("id", id)}
