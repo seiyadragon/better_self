639888d9e1199c1b06b96f70632f8ccb0d5a06fb
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2023-01-13 09:55:51 -0600

    Update tooltip.tsx

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -5,8 +5,8 @@ type TooltipProps = {
 
 const Tooltip = ({tooltip}: TooltipProps) => {
     return (
-        <p className="absolute text-white text-xs px-4 my-2 right-3 bg-zinc-900 opacity-80">
-            <i>{tooltip}</i>
+        <p className="absolute text-white text-xs my-2 right-3 bg-zinc-900 opacity-80">
+            <i className="opacity-100">{tooltip}</i>
         </p>
     )
 }; export default Tooltip;
