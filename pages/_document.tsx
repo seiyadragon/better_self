61eb12f94d838ecb6867cccb93eca5617f95225b
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2023-01-14 18:30:17 -0600

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu&display=optional"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu"></link>
       </Head>
       <body>
         <Main />
