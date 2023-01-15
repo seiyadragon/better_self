8669811f558f7d2970360f317eb645d983e27bdf
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2023-01-14 22:39:48 -0600

    0.4.0

--- components/habit.tsx
+++ components/habit.tsx
@@ -25,10 +25,10 @@ const Habit = ({name, days, id}: HabitProps) => {
     })
 
     return (
-        <section className="flex text-white my-4 text-base bg-blue-900 shadow-lg rounded-full">
-            <p className="bg-blue-800 w-24 h-36 py-4 px-4 text-center overflow-x-hidden shadow-lg rounded-full">{finalName}</p>
+        <section className="flex text-white my-4 text-base bg-blue-900 shadow-lg rounded-full border-t-2 border-b-2 border-blue-600">
+            <p className="bg-blue-800 w-24 h-36 py-4 px-4 text-center overflow-x-hidden shadow-lg rounded-full border-l-2 border-orange-400">{finalName}</p>
             <p className="bg-blue-800 w-24 h-36 py-4 px-4 ml-4 text-center overflow-x-hidden shadow-lg rounded-full">{days} days</p>
-            <section className="ml-4 w-24 h-36 shadow-lg bg-blue-800 py-6 rounded-full">
+            <section className="ml-4 w-24 h-36 shadow-lg bg-blue-800 py-6 rounded-full border-r-2 border-orange-400">
                 <TooltipButton icon={<FaArrowUp />} tooltip="Increase" toolTipColor="bg-blue-900"
                     classOverride="hover:scale-150 transition-transform px-4 mx-6 h-1/3"
                     onClick={async () => await supabaseClient.from("UserHabits").update({days: days + 1}).eq("id", id)}
--- components/project.tsx
+++ components/project.tsx
@@ -13,9 +13,9 @@ const Project = ({project}: ProjectProps) => {
     let supabaseClient = useSupabaseClient()
 
     return (
-        <section className={"w-full lg:w-96 rounded-t-xl rounded-b-xl" + ` ${isOpen ? "bg-blue-900" : ""}`}>
-            <section className="bg-blue-900 shadow-lg hover:bg-blue-800 rounded-xl">
-                <section className="md:w-96 w-full text-lg text-white  flex rounded-t-xl"
+        <section className={"w-full lg:w-96 rounded-t-xl" + ` ${isOpen ? "" : ""}`}>
+            <section className={"bg-blue-900 shadow-lg hover:bg-blue-800 rounded-t-xl" + ` ${!isOpen ? "rounded-b-xl" : ""}`}>
+                <section className="md:w-96 w-full text-lg text-white  flex rounded-t-xl h-16"
                     onClick={() => setOpen(!isOpen)}
                 >
                     <p className={`w-full px-4 py-4 text-left ${project.completed ? "line-through" : ""}`} >
@@ -34,61 +34,65 @@ const Project = ({project}: ProjectProps) => {
                     </section>
                 }
             </section>
-            {isOpen && project.tasks !== null && project.tasks.map((task: any) => {
-                if (task.name.length > 35)
-                    task.name = task.name.slice(0, 35).concat("...")
+            { isOpen && 
+                <section className="bg-blue-900 py-2">
+                    {project.tasks !== null && project.tasks.map((task: any) => {
+                        if (task.name.length > 35)
+                            task.name = task.name.slice(0, 35).concat("...")
 
 
-                return (
-                    <section key={task.name} 
-                        className={
-                            `text-white shadow-lg md:w-96 w-full px-4 flex gap-1 h-16 border-b-2 border-blue-600 rounded-full my-2
-                            ${task.completed ? "line-through bg-blue-500" : "bg-blue-800"}`
-                        }>
-                        <input type="checkbox" checked={task.completed} 
-                            className="w-8 h-16 transition-transform hover:scale-125"
-                            onChange={async () => {
-                                task.completed = !task.completed
-                                
-                                let undoneTasks = false
-                                project.tasks.map((task2: any) => {
-                                    if (!task2.completed)
-                                        undoneTasks = true
-                                })
+                        return (
+                            <section key={task.name} 
+                                className={
+                                    `text-white shadow-lg md:w-96 w-full px-4 flex gap-1 h-16 border-b-2 border-blue-600 rounded-2xl my-2
+                                    ${task.completed ? "line-through bg-blue-500" : "bg-blue-800"}`
+                                }>
+                                <input type="checkbox" checked={task.completed} 
+                                    className="w-8 h-16 transition-transform hover:scale-125"
+                                    onChange={async () => {
+                                        task.completed = !task.completed
+                                        
+                                        let undoneTasks = false
+                                        project.tasks.map((task2: any) => {
+                                            if (!task2.completed)
+                                                undoneTasks = true
+                                        })
 
-                                if (!undoneTasks)
-                                    project.completed = true
-                                else
-                                    project.completed = false
+                                        if (!undoneTasks)
+                                            project.completed = true
+                                        else
+                                            project.completed = false
 
-                                await supabaseClient.from("UserProjects").update(project).eq("id", project.id)
-                            }
-                        }/>
-                        <TooltipButton icon={<FaBan />} tooltip="Remove" toolTipColor="bg-blue-900" classOverride="hover:scale-125 transition-transform px-4 my-4 text-3xl translate-y-0.5"
-                            onClick={
-                                async (event) => {
-                                    let newTasks: Array<any> = []
+                                        await supabaseClient.from("UserProjects").update(project).eq("id", project.id)
+                                    }
+                                }/>
+                                <TooltipButton icon={<FaBan />} tooltip="Remove" toolTipColor="bg-blue-900" classOverride="hover:scale-125 transition-transform px-4 my-4 text-3xl translate-y-0.5"
+                                    onClick={
+                                        async (event) => {
+                                            let newTasks: Array<any> = []
 
-                                    project.tasks.map((task2: any) => {
-                                        if (task.name !== task2.name)
-                                            newTasks.push(task2)
-                                    })
+                                            project.tasks.map((task2: any) => {
+                                                if (task.name !== task2.name)
+                                                    newTasks.push(task2)
+                                            })
 
-                                    project.tasks = newTasks
+                                            project.tasks = newTasks
 
-                                    await supabaseClient.from("UserProjects").update(project).eq("id", project.id)
-                                }
-                            }
-                        />
-                        <p className="translate-y-5">{task.name}</p>
-                    </section>
-                )
-            })}
+                                            await supabaseClient.from("UserProjects").update(project).eq("id", project.id)
+                                        }
+                                    }
+                                />
+                                <p className="translate-y-5">{task.name}</p>
+                            </section>
+                        )
+                    })}
+                </section>
+            }
             {isOpen &&
-                <section className="text-black bg-blue-900 shadow-lg md:w-96 w-full px-4 py-4 text-lg rounded-b-full">
+                <section className="text-black bg-blue-900 md:w-96 w-full py-4 text-lg">
                     <section className="flex bg-white text-black">
                         <input 
-                            className="w-full bg-white outline-none px-2 text-lg"
+                            className="w-full bg-white outline-none px-4 text-lg rounded-xl"
                             placeholder="Type the name of your task here!"
                             value={inputValue}
                             onChange={(event) => {
@@ -108,6 +112,10 @@ const Project = ({project}: ProjectProps) => {
                             }
                         />
                     </section>
+                </section>
+            }
+            {isOpen &&
+                <section className="md:w-96 w-full text-lg shadow-xl text-white flex rounded-b-xl h-16 bg-blue-900">
                 </section>
             }
         </section>
